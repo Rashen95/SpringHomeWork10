1e8079615058dbd6e0c59c7f9ae0ad2dd741b34a
Artem Privalov <rashen95@mail.ru>
2024-02-20 21:14:28 +0300

    initial commit

---
+++ src/test/java/ru/geekbrains/SpringHomeWork10/SpringHomeWork10ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.SpringHomeWork10;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringHomeWork10ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
